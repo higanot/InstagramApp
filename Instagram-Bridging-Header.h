a4fdc12eee73dad241926dace1abfab8d3e39296
higanot <higanot555@gmail.com>
2017-04-12 19:33:30 +0900

    Header.fileの追加 project.pbxの追加

---
+++ Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by higanot on 2017/04/12.
+//  Copyright © 2017年 GitHubhiganot. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
